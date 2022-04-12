0085133a303b0e44106a0069ab2d307a5d2647c7
arstercz <arstercz@gmail.com>
2022-04-12 12:05:17 +0000

    remove HTTPTiny package and get consul data by consul-kv

--- utils/consul-kv.go
+++ utils/consul-kv.go
@@ -13,22 +13,33 @@ import (
 	"fmt"
 	"time"
 	"os"
+	b64 "encoding/base64"
+	"encoding/json"
+	"regexp"
+	"strconv"
 	"github.com/hashicorp/consul/api"
 )
+
+type MetaStatus struct {
+	Status	int
+	Flags	uint64
+	Value	string
+}
 
 func main() {
 	// options
 	var dc, server, token, key string
 	var timeout time.Duration
-	var tls bool
+	var tls, meta bool
 	var scheme string = "http"
 
 	flag.StringVar(&dc, "dc", "", "the consul datacenter")
 	flag.StringVar(&server, "server", "localhost:8500", "consul server address")
 	flag.StringVar(&token, "token", "", "the token to access address, like env variable HTTP_CONSUL_TOKEN")
 	flag.StringVar(&key, "key", "", "the key of the consul that you want get")
-	flag.DurationVar(&timeout, "timeout", 3 * time.Second, "request timeout(seconds) before give up")
+	flag.DurationVar(&timeout, "timeout", 5 * time.Second, "request timeout(seconds) before give up")
 	flag.BoolVar(&tls, "tls", false, "whether use tls or not")
+	flag.BoolVar(&meta, "meta", false, "whether get more response info or not")
 
 	flag.Parse()
 
@@ -71,16 +82,64 @@ func main() {
 		&api.QueryOptions{
 			AllowStale: true, 
 			Token: token,
+			WaitTime: timeout,
 		})
-	if err != nil {
-		fmt.Printf("error: %s\n", err.Error())
-		os.Exit(4)
-	}
-	if pair == nil {
-		fmt.Printf("Error! No key exists at: %s\n", key)
-		os.Exit(1)
-	}
 
-	fmt.Printf("%s\n", pair.Value)
+	if meta {
+		var status int = 599 // network connect timeout error
+		var flags uint64
+		var value string
+
+		if err != nil {
+			status = getResponseCode(err.Error())
+		} else {
+			if pair == nil {
+				status = 404
+			} else {
+				status = 200
+				flags  = pair.Flags
+				value  = b64.StdEncoding.EncodeToString([]byte(pair.Value))
+			}
+		}
+
+		result := MetaStatus{
+			Status:	status,
+			Flags:	flags,
+			Value:	value,
+		}
+		res, err := json.Marshal(result)
+		if err != nil {
+			fmt.Printf("error for json data: %v", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(string(res))
+	} else {
+		if err != nil {
+			fmt.Printf("error: %v\n", err.Error())
+			os.Exit(4)
+		}
+		if pair == nil {
+			fmt.Printf("Error! No key exists at: %s\n", key)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s\n", pair.Value)
+	}
 }
 
+func getResponseCode(line string) int {
+	re := regexp.MustCompile(`response code:\s+(?P<code>\d+)$`)
+	matches := re.FindStringSubmatch(line)
+
+	code := 599
+	if len(matches) > 1 {
+		var err error
+		code, err = strconv.Atoi(matches[1])
+		if err != nil {
+			code = 599
+		}
+	}
+
+	return code
+}
